78c94b02026279983d57d784ee6d56a0aa6bc4fb
morishitami <kshion.with.cad@outlook.jp>
2021-12-08 20:59:40 +0900

    email→codeへの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  def configure_permitted_parameters
+    sign_up_params = [:code, :password, :password_confirmation]
+    sign_in_params = [:code, :password,]
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: sign_up_params)
+    devise_parameter_sanitizer.permit(:sign_in, keys: sign_in_params)
+    devise_parameter_sanitizer.permit(:account_update, keys: account_update)
+  end
+
   private
 
   def basic_auth
